31096bdaa7da7912556626da12fbc6af23e9b0df
1cbyc <isaacnsisong@gmail.com>
2024-09-21 21:34:14 +0100

    refactoring from c# to c

--- main.c
+++ main.c
@@ -11,6 +11,7 @@ int main() {
     int key_length = 64;
     char priv_key[65];  // give an extra space for the null terminator i wrote earlier
 
-    // 
+    // finally generate the priv key
+        generate_priv_key(priv_key, key_length);
 
 }
