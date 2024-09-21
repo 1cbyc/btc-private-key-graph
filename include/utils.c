27f002c7e2c0dd4aa9c541b0149908a95e3a8a04
1cbyc <isaacnsisong@gmail.com>
2024-09-21 21:41:44 +0100

    refactoring from c# to c

--- include/utils.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include <time.h>
-#include "utils.h"
-
-// this way, we get a random number between 0 and limit -1
-int get_random_number(int limit) {
-    return rand() % limit;
-}
