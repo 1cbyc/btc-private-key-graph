5d0e2bdfc9136faa76006d3a02a179ac7783177a
1cbyc <isaacnsisong@gmail.com>
2024-09-21 21:29:12 +0100

    refactoring from c# to c

--- keygen.c
+++ keygen.c
@@ -5,3 +5,14 @@
 #include "utils.h"
 
 // my intent is to write the logic for the priate key generation here:
+void generate_priv_key(char *output, int length) {
+    const char validChars[] = "QWERTYUIOPASDFGHJKLZXCVBNM1234567890qwertyuiopasdfghjklzxcvbnm";
+    int validCharsLen = sizeof(validChars) - 1;
+
+    for (int i = 0; i < length; i++) {
+        int randomIndex = get_random_number(validCharsLen);
+        output[i] = validChars[randomIndex];
+    }
+
+    output[length] = '\0'; // Null-terminate the string
+}
