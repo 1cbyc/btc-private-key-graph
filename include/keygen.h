18c05560393adc87758e8403513a7eae7ee62291
1cbyc <isaacnsisong@gmail.com>
2025-08-01 00:51:14 -0700

    refactoring the utils

--- include/keygen.h
+++ include/keygen.h
@@ -1,7 +1,31 @@
 #ifndef KEYGEN_H
 #define KEYGEN_H
 
-// the idea is to generate a random private key first
-void generate_priv_key(char *output, int length);
+#include <stdint.h>
+#include <stddef.h>
+#include "crypto.h"
+
+typedef enum {
+    OUTPUT_FORMAT_HEX,
+    OUTPUT_FORMAT_WIF,
+    OUTPUT_FORMAT_BINARY
+} output_format_t;
+
+typedef struct {
+    int count;
+    output_format_t format;
+    int with_address;
+    int compressed;
+    int testnet;
+    int verbose;
+    int quiet;
+} keygen_options_t;
+
+int generate_bitcoin_key_pair(private_key_t *private_key, public_key_t *public_key, const keygen_options_t *options);
+int generate_multiple_keys(int count, const keygen_options_t *options);
+int print_key_information(const private_key_t *private_key, const public_key_t *public_key, const bitcoin_address_t *address, const keygen_options_t *options);
+int parse_command_line_args(int argc, char *argv[], keygen_options_t *options);
+void print_usage(const char *program_name);
+void print_version(void);
 
 #endif
--- include/utils.h
+++ include/utils.h
@@ -1,7 +1,30 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-// making clear the function to return a random number up to a limit
-int get_random_number(int limit);
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+#define MAX_HEX_STRING_SIZE 65
+#define MAX_WIF_STRING_SIZE 52
+#define MAX_ADDRESS_STRING_SIZE 35
+
+int hex_to_bytes(const char *hex, uint8_t *bytes, size_t bytes_size);
+int bytes_to_hex(const uint8_t *bytes, size_t bytes_size, char *hex, size_t hex_size);
+int validate_hex_string(const char *hex);
+int validate_wif_string(const char *wif);
+int reverse_bytes(uint8_t *data, size_t size);
+int compare_bytes(const uint8_t *a, const uint8_t *b, size_t size);
+bool is_zero_bytes(const uint8_t *data, size_t size);
+int safe_memcpy(void *dest, size_t dest_size, const void *src, size_t src_size);
+int safe_memset(void *dest, int value, size_t size);
+void print_hex(const uint8_t *data, size_t size);
+void print_binary(const uint8_t *data, size_t size);
+int string_to_lower(char *str);
+int string_to_upper(char *str);
+int trim_string(char *str);
+bool string_equals(const char *a, const char *b);
+bool string_starts_with(const char *str, const char *prefix);
+bool string_ends_with(const char *str, const char *suffix);
 
 #endif
